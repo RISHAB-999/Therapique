dff8dedbada6d8f8c9b0899b994ab1337b3e6dd6
Ishaan Jain <ishaanj2612@gmail.com>
2025-08-30 09:51:00 +0530

    Added ReadMe

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -27,14 +27,14 @@ const Footer = () => {
                 <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>+1-212-456-7890</li>
-                    <li>greatstackdev@gmail.com</li>
+                    <li>therapiue@gmail.com</li>
                 </ul>
             </div>
         </div>
         {/* -------- Copyright Text -------- */}
         <div>
             <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2025@ GreatStack - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright 2025@ Therapiue - All Right Reserved.</p>
         </div>
     </div>
   )
