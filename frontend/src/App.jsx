867f87ef4319ed5813826ef41fa6dffefa680771
Tarun <tarunpandore8@gmail.com>
2025-09-14 11:11:00 +0530

    Fixed the Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,24 +21,26 @@ const App = () => {
   const hideLayoutRoutes = ["/login", "/verify"];
   const shouldHideLayout = hideLayoutRoutes.includes(location.pathname);
   return (
-    <div className='mx-4 sm:mx-[10%]'>
-      <ToastContainer />
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/doctors' element={<Doctors />} />
-        <Route path='/doctors/:speciality' element={<Doctors />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/appointment/:docId' element={<Appointments />} />
-        <Route path='/my-appointments' element={<MyAppointments />} />
-        <Route path='/my-profile' element={<MyProfile />} />
-        <Route path='/coins-shop' element={<CoinsShop />} />
-        <Route path='/verify' element={<Verify />} />
-      </Routes>
+    <>
+      <div className='mx-4 sm:mx-[10%]'>
+        <ToastContainer />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/doctors' element={<Doctors />} />
+          <Route path='/doctors/:speciality' element={<Doctors />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/appointment/:docId' element={<Appointments />} />
+          <Route path='/my-appointments' element={<MyAppointments />} />
+          <Route path='/my-profile' element={<MyProfile />} />
+          <Route path='/coins-shop' element={<CoinsShop />} />
+          <Route path='/verify' element={<Verify />} />
+        </Routes>
+      </div>
       {!shouldHideLayout && <Footer />}
-    </div>
+    </>
   )
 }
 
